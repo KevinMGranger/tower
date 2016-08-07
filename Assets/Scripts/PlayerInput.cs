62e4a3548026de5accc658a025e5046a3df85bca
Kevin M Granger <github@kevinmgranger.me>
2016-08-07 13:13:01 -0400

    Add simple input component
    Movement, clicking

---
+++ Assets/Scripts/PlayerInput.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerInput : MonoBehaviour
+{
+
+    public float maxSpeed;
+
+    public float currSpeed;
+
+    Rigidbody2D bod;
+
+    Vector3 linePos;
+    bool drawLine = false;
+
+    void OnValidate()
+    {
+        bod = GetComponent<Rigidbody2D>();
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+       
+    }
+
+    // Update is called once per frame
+    // TODO: FixedUpdate?
+    void Update()
+    {
+        //TODO: probably not this
+        var force = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        bod.AddForce(force);
+
+        //get current speed and display
+        var vel = bod.velocity;
+        currSpeed = vel.magnitude;
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            var pos3 = Input.mousePosition;
+            linePos = Camera.main.ScreenToWorldPoint(pos3);
+            drawLine = true;
+        }
+        if (Input.GetMouseButtonDown(1))
+        {
+            drawLine = false;
+        }
+
+        if (drawLine) Debug.DrawLine(transform.position, linePos);
+    }
+}
